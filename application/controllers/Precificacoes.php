d47b36a10bbeaf10e7c496660d2720b8500a3999
Tavilo Breno <breno_wk2@hotmail.com>
2025-02-19 23:11:19 -0300

    feat(pricing): complete edit and delete functionality

--- application/controllers/Precificacoes.php
+++ application/controllers/Precificacoes.php
@@ -49,12 +49,24 @@ class Precificacoes extends CI_Controller
 				$this->form_validation->set_rules('precificacao_numero_vagas', 'Número de Vagas', 'trim|required|integer|greater_than[0]');
 
 				if ($this->form_validation->run()) {
+					$precificacao_ativa = $this->input->post('precificacao_ativa');
+
+					if($precificacao_ativa == 0){
+						if($this->db->table_exists('estacionar')){
+							if($this->core_model->get_by_id('estacionar', array('estacionar_precificacao_id' => $precificacao_id, 'estacionar_status' => 0))){
+								$this->session->set_flashdata('error', 'Essa precificação não pode ser desativada, pois existem registros de estacionamento com ela');
+								redirect($this->router->fetch_class());
+							}
+						}
+					}
+
 					$data = elements(
 						array(
 							'precificacao_categoria',
 							'precificacao_valor_hora',
 							'precificacao_valor_mensalidade',
 							'precificacao_numero_vagas',
+							'precificacao_ativa',
 						),
 						$this->input->post()
 					);
@@ -62,6 +74,7 @@ class Precificacoes extends CI_Controller
 					$data = html_escape($data);
 
 					$this->core_model->update('precificacoes', $data, array('precificacao_id' => $precificacao_id));
+					$this->session->set_flashdata('sucesso', 'Dados salvos com sucesso');
 					redirect($this->router->fetch_class());
 				} else {
 					$data = array(
@@ -98,4 +111,25 @@ class Precificacoes extends CI_Controller
 			return true;
 		}
 	}
+
+	public function del($precificacao_id = null)
+	{
+		// Verifica se a precificação existe
+		if (!$this->core_model->get_by_id('precificacoes', array('precificacao_id' => $precificacao_id))) {
+			$this->session->set_flashdata('error', 'Precificação não encontrada');
+			redirect($this->router->fetch_class());
+		}
+
+		// Verifica se a precificação está ativa (não pode ser excluída)
+		if ($this->core_model->get_by_id('precificacoes', array('precificacao_id' => $precificacao_id, 'precificacao_ativa' => 1))) {
+			$this->session->set_flashdata('error', 'Essa precificação não pode ser excluída, pois está ativa');
+			redirect($this->router->fetch_class());
+		}
+
+		// Se passou pelas verificações, pode excluir
+		$this->core_model->delete('precificacoes', array('precificacao_id' => $precificacao_id));
+		$this->session->set_flashdata('sucesso', 'Precificação excluída com sucesso');
+		redirect($this->router->fetch_class());
+	}
+
 }
