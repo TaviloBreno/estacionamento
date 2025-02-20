3d90f291eded63187a351f3151d21303988a1d35
Tavilo Breno <breno_wk2@hotmail.com>
2025-02-19 22:29:23 -0300

    feat(pricing): add pricing edit functionality

--- application/controllers/Precificacoes.php
+++ application/controllers/Precificacoes.php
@@ -33,4 +33,69 @@ class Precificacoes extends CI_Controller
 		$this->load->view('precificacoes/index');
 		$this->load->view('layout/footer');
 	}
+
+	public function core($precificacao_id = null)
+	{
+		if (!$precificacao_id) {
+			// Lógica para criar uma nova precificação (se necessário)
+		} else {
+			if (!$this->core_model->get_by_id('precificacoes', array('precificacao_id' => $precificacao_id))) {
+				$this->session->set_flashdata('error', 'Precificação não encontrada');
+				redirect($this->router->fetch_class());
+			} else {
+				$this->form_validation->set_rules('precificacao_categoria', 'Categoria', 'trim|required|min_length[5]|max_length[30]|callback_check_categoria');
+				$this->form_validation->set_rules('precificacao_valor_hora', 'Valor por Hora', 'trim|required|max_length[10]');
+				$this->form_validation->set_rules('precificacao_valor_mensalidade', 'Valor da Mensalidade', 'trim|required|max_length[10]');
+				$this->form_validation->set_rules('precificacao_numero_vagas', 'Número de Vagas', 'trim|required|integer|greater_than[0]');
+
+				if ($this->form_validation->run()) {
+					$data = elements(
+						array(
+							'precificacao_categoria',
+							'precificacao_valor_hora',
+							'precificacao_valor_mensalidade',
+							'precificacao_numero_vagas',
+						),
+						$this->input->post()
+					);
+
+					$data = html_escape($data);
+
+					$this->core_model->update('precificacoes', $data, array('precificacao_id' => $precificacao_id));
+					redirect($this->router->fetch_class());
+				} else {
+					$data = array(
+						'titulo' => 'Atualizar Precificação',
+						'subtitulo' => 'Aqui você pode atualizar a precificação',
+						'icone_view' => 'ik ik-dollar-sign',
+						'styles' => array(
+							'plugins/mask/jquery.mask.min.css',
+						),
+						'scripts' => array(
+							'plugins/mask/jquery.mask.min.js',
+							'plugins/mask/app.js',
+						),
+						'categoria' => $this->core_model->get_by_id('precificacoes', array('precificacao_id' => $precificacao_id)),
+						'precificacao' => $this->core_model->get_by_id('precificacoes', array('precificacao_id' => $precificacao_id)),
+					);
+
+					$this->load->view('layout/header', $data);
+					$this->load->view('precificacoes/core');
+					$this->load->view('layout/footer');
+				}
+			}
+		}
+	}
+
+	public function check_categoria($precificacao_categoria)
+	{
+		$precificacao_id = $this->input->post('precificacao_id');
+
+		if ($this->core_model->get_by_id('precificacoes', array('precificacao_categoria' => $precificacao_categoria, 'precificacao_id !=' => $precificacao_id))) {
+			$this->form_validation->set_message('check_categoria', 'Essa categoria já existe');
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
