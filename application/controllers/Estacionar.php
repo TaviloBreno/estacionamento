265833a608a34367135fc7ea5c59fc3fe555d5cc
Tavilo Breno <breno_wk2@hotmail.com>
2025-03-06 00:54:13 -0300

    feat: Closing parking ticket - Part 2

--- application/controllers/Estacionar.php
+++ application/controllers/Estacionar.php
@@ -48,29 +48,60 @@ class Estacionar extends CI_Controller
 				$this->session->set_flashdata('error', 'Ticket de estacionamento não encontrado para encerramento!');
 				redirect('estacionar');
 			}else{
-				$data = array(
-					'titulo' => 'Encerrar Ticket de Estacionamento',
-					'subtitulo' => 'Encerrando ticket de estacionamento',
-					'texto_modal' => 'Deseja realmente encerrar o ticket de estacionamento?',
-					'valor_btn' => 'Encerrar Ticket',
-					'icone_view' => 'ik ik-truck',
-					'scripts' => array(
-						'mask/jquery.mask.min.js',
-						'mask/estacionar.js',
-						'plugins/datatables.net/js/jquery.dataTables.min.js',
-						'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
-						'plugins/datatables.net/js/estacionamento.js',
-						'plugins/datatables.net/js/flashcards.js',
-						'js/estacionar/estacionar.js'
-					),
-					'estacionado' => $this->core_model->get_by_id('estacionar', array('estacionar_id' => $estacionar_id)),
-					'precificacoes' => $this->core_model->get_all('precificacoes', array('precificacao_ativa' => 1)),
-					'formas_pagamentos' => $this->core_model->get_all('formas_pagamentos', array('forma_pagamento_ativa' => 1)),
-				);
+				$estacionar_tempo_decorrido = str_replace('.', '', $this->input->post('estacionar_tempo_decorrido'));
 
-				$this->load->view('layout/header', $data);
-				$this->load->view('estacionar/core');
-				$this->load->view('layout/footer');
+				if($estacionar_tempo_decorrido > '015'){
+					$this->form_validation->set_rules('estacionar_forma_pagamento_id', 'Forma de pagamento', 'trim|required');
+				}
+
+				if($this->form_validation->run()) {
+					$data = elements(
+						array(
+							'estacionar_valor_devido',
+							'estacionar_forma_pagamento_id',
+							'estacionar_tempo_decorrido',
+						), $this->input->post()
+					);
+
+					if ($data['estacionar_tempo_decorrido'] <= '015') {
+						$data['estacionar_forma_pagamento_id'] = 5;
+					}
+
+					$data['estacionar_data_saida'] = date('Y-m-d H:i:s');
+					$data['estacionar_status'] = 1;
+
+					$data = html_escape($data);
+
+					$this->core_model->update('estacionar', $data, array('estacionar_id' => $estacionar_id));
+					redirect($this->router->fetch_class());
+
+				}else{
+
+					$data = array(
+						'titulo' => 'Encerrar Ticket de Estacionamento',
+						'subtitulo' => 'Encerrando ticket de estacionamento',
+						'texto_modal' => 'Deseja realmente encerrar o ticket de estacionamento?',
+						'valor_btn' => 'Encerrar Ticket',
+						'icone_view' => 'ik ik-truck',
+						'scripts' => array(
+							'mask/jquery.mask.min.js',
+							'mask/estacionar.js',
+							'plugins/datatables.net/js/jquery.dataTables.min.js',
+							'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+							'plugins/datatables.net/js/estacionamento.js',
+							'plugins/datatables.net/js/flashcards.js',
+							'js/estacionar/estacionar.js'
+						),
+						'estacionado' => $this->core_model->get_by_id('estacionar', array('estacionar_id' => $estacionar_id)),
+						'precificacoes' => $this->core_model->get_all('precificacoes', array('precificacao_ativa' => 1)),
+						'formas_pagamentos' => $this->core_model->get_all('formas_pagamentos', array('forma_pagamento_ativa' => 1)),
+					);
+
+					$this->load->view('layout/header', $data);
+					$this->load->view('estacionar/core');
+					$this->load->view('layout/footer');
+
+				}
 			}
 		}
 	}
